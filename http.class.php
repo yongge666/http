d270af28e85098506186de204be4fa508d494c0e
yongge666 <2550702985@qq.com>
2016-03-08 23:43:11 +0800

    完善http类之get请求

--- http.class.php
+++ http.class.php
@@ -9,9 +9,9 @@
  */
 
 //http请求类的接口
-interface proto{
+interface Proto{
     //分析url
-    public function connect();
+    public function connect($url);//子类必须带参数
 
     //发送get请求
     public function get();
@@ -24,48 +24,95 @@ interface proto{
 
 }
 
-class http implements proto{
+class Http implements Proto{
+    //声明换行符
+    const CRLF = '\r\n'; //(carriage return line feed)
     //保存相关属性值
     protected $urlInfo=array();
+    protected $version = 'HTTP/1.1';
     protected $line=array();
     protected $header=null;
     protected $body=null;
+    protected $fh=null;
 
-    //写请求行
+    protected $errno=null;
+    protected $errstr=null;
+    protected $outtime=3;
+
+    protected $response;
+
+    public function __construct($url)
+    {
+        $this->connect($url);
+        $this->setHeader('Host: '.$this->urlInfo['host']);
+    }
+
+    //构造请求行
     protected function setLine($method){
-        $this->line[0]=$method.' ';
+        $this->line[0]=$method.' '.$this->urlInfo['path'].' '.$this->version;
 
     }
 
-    //写头信息
-    protected function setHeader(){
-
+    //构造头信息
+    protected function setHeader($headerLine){
+        $this->header[]=$headerLine;
     }
 
-    //写主体信息
+    //构造主体信息
     protected function setBody(){
 
     }
 
-    //分析url
+    //分析和连接url
     public function connect($url){
 
         $this->urlInfo=parse_url($url);
+        //scheme - 如 http,host,port,user,pass,path,query - 在问号 ? 之后,fragment - 在散列符号 # 之后
+        //判断端口
+        if(!isset($this->urlInfo['port'])){
+            $this->urlInfo['port'] = 80;
+        }
+        //连接url
+        $this->fh = fsockopen($this->urlInfo['host'],$this->urlInfo['port'],$this->errno,$this->errstr,$this->outtime);
     }
 
-    //发送get请求
+    //构造get请求发送的数据
     public function get(){
         $this->setLine('GET');
+        $this->request();
+        return $this->response;
+        echo $this->setLine('GET');
 
     }
 
-    //发送post请求
+    //构造post请求发送的数据
     public function post(){
 
     }
 
-    //关闭连接
+    //发送请求
+    public function request(){
+        //把请求行，头信息，实体信息，放在一个数组里，便于拼接
+        $req = array_merge($this->line,$this->header,array(),$this->body,array());
+        $req = implode(self::CRLF,$req);
+        fwrite($this->fh,$req);
+        while(!feof($this->fh)){
+           $this->response .= fread($this->fh,1024);
+        }
+
+        $this->close();
+        //return $this->response;
+        print_r($req);
+    }
+  //关闭连接
     public function close(){
 
     }
+
+
 }
+
+$url = 'http://news.163.com/16/0308/19/BHLJJ94H00014PRF.html';
+$http = new Http($url);
+echo $http->get();
+var_dump($http);
